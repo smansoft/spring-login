2f0427304cf549dd412909f725dd795a372c2022
SMan <sman@smansoft.com>
2019-04-15 10:02:32 +0300

    sql scripts: have been added for mysql 8 and mysql 5;

---
+++ scripts/mysql/create_database.sql
@@ -0,0 +1,8 @@
+
+create database spring_login_1 character set 'utf8' collate 'utf8_unicode_ci';
+create database spring_login_2 character set 'utf8' collate 'utf8_unicode_ci';
+create database spring_login_3 character set 'utf8' collate 'utf8_unicode_ci';
+create database spring_login_4 character set 'utf8' collate 'utf8_unicode_ci';
+create database spring_login_5 character set 'utf8' collate 'utf8_unicode_ci';
+create database spring_login_test character set 'utf8' collate 'utf8_unicode_ci';
+
